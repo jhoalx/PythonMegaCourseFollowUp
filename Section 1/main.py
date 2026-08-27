97e38c4c9755458464f9c35ef1b6b1e9be5c31e5
John Alejandro Zapata <jhoalx@gmail.com>
2026-08-27 17:37:31 -0500

    8. Storing User Input in Lists

--- Section 1/main.py
+++ Section 1/main.py
@@ -1,2 +1,10 @@
-user_prompt = input("Please enter a To-do: ")
-print(user_prompt)
+user_prompt = "Please enter a To-do: "
+
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3, "hello"]
+print(todos)
+
+print(type(todo1))
