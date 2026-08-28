e92f8c7b77583e6e436f25b1bc4aff6e8d514d21
John Alejandro Zapata <jhoalx@gmail.com>
2026-08-27 21:21:30 -0500

    22. Storing User Input Repeatedly

--- Section 2/main.py
+++ Section 2/main.py
@@ -1,6 +1,9 @@
 user_prompt = 'Please enter a To-do: '
 
+todos = []
+
 while True:
     todo = input(user_prompt)
-    print(todo)
-    print("Next...")
+    todo = todo.capitalize()  # Capitalize the input before appending
+    todos.append(todo)
+    print(todos)
