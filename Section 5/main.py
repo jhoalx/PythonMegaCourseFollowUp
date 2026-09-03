d8c35c09378e8d8a836021753ddba77a66329340
John Alejandro Zapata <jhoalx@gmail.com>
2026-09-03 17:10:56 -0500

    53. Numbered Todos

---
+++ Section 5/main.py
@@ -0,0 +1,22 @@
+todos = []
+
+while True:
+    user_action = input("type 'add', 'show', 'edit' or 'exit': ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for index, item in enumerate(todos):
+                print(index, " - ", item)
+        case 'edit':
+            index = int(input("Enter the number of the item to be edited: "))
+            todos[index] = input("Enter the new Todo: ")
+        case "exit":
+            break
+        case _:
+            print("Invalid Command")
+
+print("Bye!")
