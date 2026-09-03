ade10329509102a2a07258370c6a10e7240e9197
John Alejandro Zapata <jhoalx@gmail.com>
2026-09-03 17:45:54 -0500

    55. Add a "Complete Todo" Feature

--- Section 5/main.py
+++ Section 5/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("type 'add', 'show', 'edit' or 'exit': ")
+    user_action = input("type 'add', 'show', 'edit', 'complete' or 'exit': ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,10 +10,12 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos):
-                print(f"{index}-{item}")
+                print(f"{index + 1}-{item}")
         case 'edit':
-            index = int(input("Enter the number of the item to be edited: "))
+            index = int(input("Enter the number of the item to be edited: ")) - 1
             todos[index] = input("Enter the new Todo: ")
+        case "complete":
+            todos.pop(int(input("Enter the number of the item completed: ")) - 1)
         case "exit":
             break
         case _:
