c28143d004fb37dd7fb88a5036c46ef31e2e99ad
John Alejandro Zapata <jhoalx@gmail.com>
2026-09-08 15:18:47 -0500

    368. Implementing Continuous Conversations

--- Section_41_App12_AI_Agent_With_LangChain/main.py
+++ Section_41_App12_AI_Agent_With_LangChain/main.py
@@ -1,11 +1,16 @@
 import os
+import uuid
+
 import gradio as gr
+from anyio.lowlevel import checkpoint
 
 from dotenv import load_dotenv
 from datetime import datetime
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 # from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 load_dotenv(verbose=True)
 
@@ -16,6 +21,9 @@ def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
 
+
+conn = sqlite3.connect("agent_memory.sqlite", check_same_thread=False)
+checkpointer = SqliteSaver(conn)
 
 # llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
 
@@ -32,16 +40,19 @@ use the get_date tool if the uer is asking about the today's date
 agent = create_agent(
     model=llm,
     system_prompt=system_prompt,
+    checkpointer=checkpointer,
     tools=[
         get_date
     ]
 )
 
+
 # user_query = input("Enter a Query: ")
 
 # print("AI Answer:", ai_answer)
 
-def chat(message, history):
+def chat(message, history, thread_id):
+    config = {"configurable": {"thread_id": thread_id}}
     response = agent.invoke({
         "messages": [
             {
@@ -49,7 +60,9 @@ def chat(message, history):
                 "content": message
             }
         ]
-    })
+    },
+        config=config
+    )
 
     all_messages = response["messages"]
     # ai_answer = all_messages[-1].content[0]['text']
@@ -59,6 +72,7 @@ def chat(message, history):
 
 with gr.Blocks() as demo:
     gr.Markdown("# AI Agent")
-    gr.ChatInterface(fn=chat)
+    thread_id = gr.State(value = lambda: str(uuid.uuid4()))
+    gr.ChatInterface(fn=chat, additional_inputs=[thread_id])
 
 demo.launch()
